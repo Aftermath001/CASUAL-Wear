f3d4dc4883070b111effd7520126c61879adb989
Aftermath001 <alvinasiachi@gmail.com>
2024-03-26 22:27:20 +0300

    Slider

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import SearchIcon from '@mui/icons-material/Search';
 import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,8 +1,20 @@
+import  Slider  from '../../components/Slider/Slider'
 import React from 'react'
+import './Home.scss'
+// import FeaturedProducts from '../../components/FeaturedProducts/FeaturedProducts'
+// import Categories from '../../components/Categories/Categories'
+// import Contact from '../../components/Contact/Contact'
+
 
 const Home = () => {
   return (
-    <div>Home</div>
+   <div className="home">
+      <Slider />
+      {/* <FeaturedProducts type='Featured'/>
+      <Categories/>
+      <FeaturedProducts type='Trending'/>
+      <Contact /> */}
+   </div>
   )
 }
 
